2b30014757a97890bbfa983217e25cb18bdc6d50
wedump-ace <kwy2000@lycos.co.kr>
2013-08-29 17:39:29 +0900

    Added wedump-import.js for package and class load

---
+++ js/wedump-import.js
@@ -0,0 +1,13 @@
+// 패키지 설정
+var wedump = {};
+wedump.core = {};
+wedump.core.drawingEngine = {};
+wedump.core.drawingEngine.detail = {};
+wedump.core.drawingEngine.sketch = {};
+
+// 클래스 Import
+document.write('<script type="text/javascript" src="wedump/js/annex/jquery-2.0.3.min.js"></script>');
+document.write('<script type="text/javascript" src="wedump/js/core/drawingEngine/detail/DetailControl.js"></script>');
+document.write('<script type="text/javascript" src="wedump/js/core/drawingEngine/sketch/Sketch.js"></script>');
+document.write('<script type="text/javascript" src="wedump/js/core/drawingEngine/sketch/SketchBase.js"></script>');
+document.write('<script type="text/javascript" src="wedump/js/Wedump.js"></script>');
