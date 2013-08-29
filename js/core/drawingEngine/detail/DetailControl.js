5290495eee8d91f953e21c16c7ecf14215b8385d
wedump-ace <kwy2000@lycos.co.kr>
2013-08-29 17:41:53 +0900

    Delete package info

--- js/core/drawingEngine/detail/DetailControl.js
+++ js/core/drawingEngine/detail/DetailControl.js
@@ -1,11 +1,6 @@
-var wedump = {};
-wedump.core = {};
-wedump.core.drawingEngine = {};
-wedump.core.drawingEngine.detail = {};
-
 /**
  * DetailControl : Sketch 후 위치에 대한 상세제어를 제공
- * @type {Class}
+ * @type {Implementation Class}
  */
 wedump.core.drawingEngine.detail.DetailControl = function() {
 	var public = wedump.core.drawingEngine.detail.DetailControl.prototype;
